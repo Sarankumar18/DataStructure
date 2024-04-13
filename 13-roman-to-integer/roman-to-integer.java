e540eaac9789b882e4223697dc59e8bc03055dda
Sarankumar Baskar <saranbaskar121@gmail.com>
2024-04-13 23:37:08 +0530

    Time: 5 ms (59.22%) | Memory: 44.6 MB (48.18%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,31 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
         Map<Character, Integer> roman = new HashMap<>();
-        roman.put('I', 1);
-        roman.put('V', 5);
-        roman.put('X', 10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
-        
-        int romanValue = 0;
-        int prevValue = 0; // Keep track of previous value
-        
-        for (int i = s.length() - 1; i >= 0; i--) {
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);
+        roman.put('D',500);
+        roman.put('M',1000);
+        int  romanValue = 0;
+        int prevValue = 0;
+        for (int i = s.length()-1 ; i >= 0; i--){
             char c = s.charAt(i);
             int value = roman.get(c);
-            
-            // If current value is less than previous value, subtract it
-            if (value < prevValue) {
+            if (value < prevValue){
                 romanValue -= value;
-            } else {
+            }else{
                 romanValue += value;
             }
-            
-            prevValue = value; // Update previous value
+            prevValue = value;
         }
-        
         return romanValue;
     }
 }
