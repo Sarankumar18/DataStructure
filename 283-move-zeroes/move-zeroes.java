8acfea0de8f1caf655d37420c9608347057a0cba
Sarankumar Baskar <saranbaskar121@gmail.com>
2024-04-19 05:12:48 +0530

    Time: 2 ms (85.63%) | Memory: 46.4 MB (10.46%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
     int leftMostZeroIndex = 0; 
-
-    // Iterate through the array
-    for (int i = 0; i < nums.length; i++) {
-        // If the current element is not zero
-        if (nums[i] != 0) {
-            // Swap the current element with the leftmost zero element (if any)
+    for(int i=0; i<nums.length; i++){
+        if(nums[i] != 0){
             int temp = nums[i];
             nums[i] = nums[leftMostZeroIndex];
             nums[leftMostZeroIndex] = temp;
-
-            // Move the leftmost zero index to the next position
             leftMostZeroIndex++;
         }
     }
